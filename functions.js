ef2cad42c058dda41b6787ebe7680238f51c7873
Becca <beccasafan@gmail.com>
2019-12-05 23:05:15 -0600

    20191206-050515

--- functions.js
+++ functions.js
@@ -2,8 +2,8 @@
 
 //var square_banners = ["square-ad-00.png","square-ad-01.png","square-ad-02.png"];
 //var horiz_banners = ["horiz-ad-00.png","horiz-ad-01.png","horiz-ad-02.png"];
-var square_banners = ["ay-square-00.png"];
-var horiz_banners = ["ay-horiz-00.png"];
+var square_banners = ["ay-square-01.png"];
+var horiz_banners = ["ay-horiz-01.png"];
 
 function getRandomSquare(imgAr, path) {
     path = path || '../images/'; 
